41c3cb0ed31ab434c179676109a563c320d05284
DoloresDai <841856753@qq.com>
2019-01-24 15:04:53 +0800

    Complete detailpage search

---
+++ js/search_get_movies.js
@@ -0,0 +1,22 @@
+$(function() {
+  let itemsStorage = new ItemsStorage(localStorage);
+  itemsStorage.storageItems(MOVIESDATA);
+  var typeArr = TYPE;
+  var movie = localStorage.getItem("type");
+  searchMain(movie, typeArr, itemsStorage)
+  listenerOfSort(itemsStorage);
+});
+
+function listenerOfSort(itemsStorage) {
+  $(".filter_nav li a").on("click", function(event) {
+    $(this).addClass('selected').siblings('a').removeClass('selected');
+    $(this).parent('li').siblings('li').find('a').removeClass('selected');
+    let filterWord = $(this).text().trim();
+    if (filterWord == '全部') {
+      showMovies(itemsStorage.getStorageItems());
+    } else {
+      let filterMovies = itemsStorage.filterItems(filterWord);
+      showMovies(filterMovies);
+    }
+  });
+}
